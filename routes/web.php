a6d2bb9ad4e138814b8c76f93d37c6a802ab08ed
Ankana <reach2ankana@gmail.com>
2024-06-17 12:32:15 +0530

    Athentication

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,29 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('account/register',[AccountController::class,'registration'])->name('account.registration');
-Route::post('account/proccess-register',[AccountController::class,'proccessRegistration'])->name('account.proccessRegistration');
-Route::get('account/login',[AccountController::class,'login'])->name('account.login');
+
+
+
+
+
+
+Route::group(['account'],function(){
+    //Guest route
+
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('account/register',[AccountController::class,'registration'])->name('account.registration');
+    Route::post('account/proccess-register',[AccountController::class,'proccessRegistration'])->name('account.proccessRegistration');
+    Route::get('account/login',[AccountController::class,'login'])->name('account.login');
+    Route::post('account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
+});
+
+//Authenticate route
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('account/profile',[AccountController::class,'profile'])->name('account.profile');
+    Route::get('account/logout',[AccountController::class,'logout'])->name('account.logout');
+
+
+});
+
+});
