11e444dff1e44b05569ae1987ff613ed1169ba8c
Ankana <reach2ankana@gmail.com>
2024-07-02 21:11:17 +0530

    job_details

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -60,4 +60,31 @@ return view('front.jobs',[
     'jobTypeArray' => $jobTypeArray
 ]);
     }
+
+    public function detail($id) {
+        $job = Job::where([
+            'id' => $id, 
+            'status' => 1
+        ])->with(['jobType','category'])->first();
+
+if ($job == null) {
+abort(404);
+}
+
+// $count = 0;
+// if (Auth::user()) {
+// $count = SavedJ::where([
+// 'user_id' => Auth::user()->id,
+// 'job_id' => $id
+// ])->count();
+// }
+
+
+// fetch applicants
+
+// $applications = JobApplication::where('job_id',$id)->with('user')->get();
+
+
+return view('front.jobDetail',['job' => $job]);
+}
 }
