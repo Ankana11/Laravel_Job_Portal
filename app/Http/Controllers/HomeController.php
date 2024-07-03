6dde4ea4a38c5dd690a6476cfc7524bc22de0b42
Ankana <reach2ankana@gmail.com>
2024-07-03 20:52:04 +0530

    job-search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,13 +12,15 @@ class HomeController extends Controller
     public function index(){
 
 $categories = Category::where('status',1)->orderBy('name','ASC')->take(8)->get();
+$newCategories = Category::where('status',1)->orderBy('name','ASC')->get();
 $jobs = Job::where('status',1)->where('isFetured',1)->orderBy('created_at','DESC')->with('jobType')->take(6)->get();
 $latestjobs = Job::where('status',1)->orderBy('created_at','DESC')->with('jobType')->take(6)->get();
 
         return view('front.home',[
             'categories' => $categories,
             'jobs' => $jobs,
-            'latestjobs' => $latestjobs
-        ]);
+            'latestjobs' => $latestjobs,
+            'newCategories' => $newCategories
+         ]);
     }
 }
